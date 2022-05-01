b5db18589f95d9df175d228b7f0287c2fd199827
Bastian Müller <bastian@turbolent.com>
2022-04-30 20:52:30 -0700

    clean up

--- serial.go
+++ serial.go
@@ -11,7 +11,6 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-
 func openSerialPort(device string) *os.File {
 	f, err := os.OpenFile(device, unix.O_RDWR|unix.O_NOCTTY|unix.O_NONBLOCK, 0666)
 	if err != nil {
@@ -20,21 +19,21 @@ func openSerialPort(device string) *os.File {
 
 	fd := C.int(f.Fd())
 	if C.isatty(fd) != 1 {
-		f.Close()
+		_ = f.Close()
 		log.Fatalf("device is not a TTY: %s", device)
 	}
 
 	var settings C.struct_termios
 	_, err = C.tcgetattr(fd, &settings)
 	if err != nil {
-		f.Close()
+		_ = f.Close()
 		log.Fatal(err)
 	}
 
 	C.cfmakeraw(&settings)
-	_, err = C.tcsetattr(fd, C.TCSANOW, &settings);
+	_, err = C.tcsetattr(fd, C.TCSANOW, &settings)
 	if err != nil {
-		f.Close()
+		_ = f.Close()
 		log.Fatal(err)
 	}
 
